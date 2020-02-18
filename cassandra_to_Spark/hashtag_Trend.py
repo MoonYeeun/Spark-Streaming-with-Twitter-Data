40cf11458fe529bee198234063f0d287606567b1
MoonYeeun <aams82@hanmail.net>
2020-02-18 15:22:30 +0900

    connect to Cassandra with Spark

---
+++ cassandra_to_Spark/hashtag_Trend.py
@@ -0,0 +1,168 @@
+import pyspark
+from pyspark.streaming import StreamingContext
+from pyspark import StorageLevel
+from pyspark.sql.types import *
+import json, time
+from itertools import chain
+
+# 해시태그 분석
+
+# import pyspark_cassandra
+# sc = pyspark.SparkConf()\
+#     .setMaster("local[*]")\
+#     .set("spark.driver.memory","8g")\
+#     .set("spark.executor.memory","8g")\
+#     .set("spark.debug.maxToStringFields", 10000) \
+#     .setMaster("spark://spark-master:7077") \
+#     .set("spark.cassandra.connection.host", "cas-1")
+# sparkContext = pyspark.SparkContext(conf=sc)
+# sc = CassandraSparkContext(conf=conf)
+
+spark = pyspark.sql.SparkSession.builder \
+    .appName("pysaprk_python") \
+    .config('spark.cassandra.connection.host', '10.240.14.37') \
+    .config('spark.cassandra.connection.port', '9042') \
+    .getOrCreate()
+# myRedis = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+# cassandra 에서 받아오는 data schema
+schema = StructType([
+    StructField("created_at", StringType(), False),
+    StructField("id", LongType(), False),
+    StructField("truncated", BooleanType(), False),
+    StructField("retweeted", BooleanType(), False),
+    StructField("reply_status", BooleanType(), False),
+    StructField("is_quote_status", BooleanType(), False),
+    StructField("media_status", BooleanType(), False),
+    StructField("text", StringType(), False),
+    StructField("user", StringType(), False),
+    StructField("retweeted_status", StringType(), True),
+    StructField("quoted_status", StringType(), True),
+    StructField("hashtags", ArrayType(), False),
+    StructField("entities", StringType(), False),
+    StructField("extended_entities", StringType(), True),
+    StructField("extended_tweet", StringType(), True),
+    StructField("lang", StringType(), False),
+    StructField("date", StringType(), False),
+    StructField("timestamp", LongType(), False)
+])
+
+# 분석 대상에서 제외할 단어 명시
+mystopwords = [
+    'BTS',
+    'bts',
+    'Bts',
+    '방탄소년단'
+]
+# 유사어 설정
+similarwords = [
+    ['정국', 'JUNGKOOK'],
+    ['지민', 'JIMIN'],
+    ['제이홉', 'JHOPE'],
+    ['슈가', 'SUGA'],
+    ['김태형', '태형', '뷔', 'TAEHYUNG'],
+    ['김남준', '남준', '랩몬', 'RM'],
+    ['김석진', '석진', '진', 'SEOKJIN', 'JIN']
+]
+
+
+# get DStream dataframe
+def get_streaming(data, schema=None):
+    process_df(data)
+    return True
+
+
+def process_df(data):
+    rdd = data.rdd.map(lambda value: json.loads(value[0])) \
+        .map(lambda v: v['hashtags']).collect()
+    result = []
+    for i in rdd:
+        temp = []
+        for j in i:
+            temp.append(j['text'])
+        result.append(temp)
+    # print(result)
+    processing_result = process_hashtag(result)
+    # word count 작업을 위해 결과(list) rdd로 만들어줌
+    rdd = spark.sparkContext.parallelize(processing_result, 2)
+    word_count(rdd)
+
+
+# 해시태그 전처리
+def process_hashtag(text):
+    del_similar = []
+    result = []
+    # 각 tweet 별 유사어 제거
+    for v in text:
+        if not v:
+            continue
+        words = '-'.join(v)
+        words = words.upper()
+        temp = []
+        for i in similarwords[0]:
+            if i in words:
+                words = words.replace(i, 'Jungkook')
+        for i in similarwords[1]:
+            if i in words:
+                words = words.replace(i, 'Jimin')
+        for i in similarwords[2]:
+            if i in words:
+                words = words.replace(i, 'JHope')
+        for i in similarwords[3]:
+            if i in words:
+                words = words.replace(i, 'Suga')
+        for i in similarwords[4]:
+            if i in words:
+                words = words.replace(i, 'Taehyung')
+        for i in similarwords[5]:
+            if i in words:
+                words = words.replace(i, 'RM')
+        for i in similarwords[6]:
+            if i in words:
+                words = words.replace(i, 'JIN')
+        temp = words.split('-')
+        temp = list(set(temp))
+        del_similar.append(temp)
+
+    total = list(chain.from_iterable(del_similar))  # 리스트 안에 리스트 하나의 리스트로 합치기
+    # 불용어 제거
+    for i in total:
+        if i not in mystopwords:
+            result.append(i)
+
+    print('응 들어옴')
+    # print(result)
+    return result
+
+
+# 추출된 단어 word count
+def word_count(list):
+    print('word count 들어옴')
+    pairs = list.map(lambda word: (word, 1))
+    # 상위 10개만 가져오기 + 등장빈도 2번 이상
+    wordCounts = pairs.reduceByKey(lambda x, y: x + y).filter(lambda args: args[1] > 2)
+    ranking = wordCounts.takeOrdered(20, lambda args: -args[1])
+    print(ranking)
+    # key : 현재 시간 , value : 순위 결과 json 으로 redis 저장
+#     current_time = time.strftime("%d/%m/%Y")
+#     rank_to_json = json.dumps(ranking)
+#     myRedis.set(current_time, rank_to_json, ex=60*5)
+
+
+if __name__ == "__main__":
+    while True:
+        # 카산드라로부터 data 불러오기  (20초마다)
+        lines = spark.read \
+            .format("org.apache.spark.sql.cassandra") \
+            .options(table="master_dataset", keyspace="bts") \
+            .load()
+        current_time = int(time.time() * 1000000)  # 현재시간 마이크로 세컨즈 까지
+        print(current_time)  # 현재시간 출력
+        # 현재 시간 부터 20초 전까지 data 불러오기
+        lines = lines.select('entities') \
+            .where((lines.timestamp >= current_time - 20000000) & (lines.timestamp <= current_time)).cache()
+        get_streaming(lines)
+        time.sleep(20)
+
+
+
---
+++ cassandra_to_Spark/tweet_saveToCassandra.py
@@ -0,0 +1,79 @@
+import pyspark
+from pyspark.streaming import StreamingContext
+from itertools import chain
+from pyspark.sql.functions import *
+from pyspark import StorageLevel
+from pyspark.sql.types import *
+import json, time
+
+spark = pyspark.sql.SparkSession.builder \
+    .appName("pysaprk_python") \
+    .config('spark.cassandra.connection.host', '10.240.14.37') \
+    .config('spark.cassandra.connection.port', '9042') \
+    .getOrCreate()
+
+schema = [
+    'retweeted as is_retweeted',
+    'retweeted_status as tweet_content',
+    'timestamp'
+]
+struct = StructType([StructField("tweet_id", LongType(), False),
+                     StructField("tweet_content", StringType(), False),
+                     StructField("tweet_favorite_count", LongType(), False),
+                     StructField("tweet_quote_count", LongType(), False),
+                     StructField("tweet_retweeted_count", LongType(), False),
+                     StructField("timestamp", LongType(), False)])
+
+
+def process_Tweet(data):
+    # df 생성에 필요한 데이터 추출 (리트윗 된 트윗 id, 트위 내용, 트윗 좋아요 개수, 트윗 인용 개수, 리트윗 개수, 작성시간)
+    tweet_content = data.select('tweet_content').rdd.flatMap(lambda value: value).collect()
+    tweet_id = data.select('tweet_content').rdd.map(lambda value: json.loads(value[0])) \
+        .map(lambda v: v['id']).collect()
+    tweet_favorite_count = data.select('tweet_content').rdd.map(lambda value: json.loads(value[0])) \
+        .map(lambda v: v['favorite_count']).collect()
+    tweet_quoted_count = data.select('tweet_content').rdd.map(lambda value: json.loads(value[0])) \
+        .map(lambda v: v['quote_count']).collect()
+    tweet_retweet_count = data.select('tweet_content').rdd.map(lambda value: json.loads(value[0])) \
+        .map(lambda v: v['retweet_count']).collect()
+    timestamp = data.select('timestamp').rdd.flatMap(lambda value: value).collect()
+
+    # 새로운 dataframe 생성
+    newDF = spark.createDataFrame(
+        zip(tweet_id, tweet_content, tweet_favorite_count, tweet_quoted_count, tweet_retweet_count, timestamp), struct)
+    newDF.show()
+
+def save_Tweet(df):
+    df.write.format("org.apache.spark.sql.cassandra")\
+    .mode('append').options(table="tweet_rank", keyspace="bts").save()
+
+
+
+if __name__ == "__main__":
+    #     lines = spark.read\
+    #         .format("org.apache.spark.sql.cassandra")\
+    #         .options(table="master_dataset", keyspace="bts")\
+    #         .load()
+    #     current_time = int(time.time()*1000000) # 현재시간 마이크로 세컨즈 까지
+    #     print(current_time) # 현재시간 출력
+    #     # 현재 시간 부터 10초 전까지 data 불러오기
+    #     lines = lines.selectExpr(schema)\
+    #     .where((lines.timestamp >= current_time-10000000) & (lines.timestamp <= current_time) & (lines.retweeted == True)).limit(10).cache()
+    #     #lines.show()
+    #     get_Tweet(lines)
+    while True:
+        # 카산드라로부터 data 불러오기 (10초 마다)
+        lines = spark.read \
+            .format("org.apache.spark.sql.cassandra") \
+            .options(table="master_dataset", keyspace="bts") \
+            .load()
+        lines.printSchema()
+        current_time = int(time.time() * 1000000)  # 현재시간 마이크로 세컨즈 까지
+        print(current_time)  # 현재시간 출력
+        # 현재 시간 부터 10초 전까지 data 불러오기
+        lines = lines.selectExpr(schema) \
+            .where((lines.timestamp >= current_time - 10000000) & (lines.timestamp <= current_time) & (
+                    lines.retweeted == True)).limit(10).cache()
+        # lines.show()
+        process_Tweet(lines)
+        time.sleep(10)
